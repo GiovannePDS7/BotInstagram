0e9df8a445447ef144b6d429b6b936e8a71fae19
Giovanne Pagano <giovanne3282@gmail.com>
2023-04-16 21:02:35 -0300

    Instruções de uso

--- botInsta.py
+++ botInsta.py
@@ -1,3 +1,6 @@
+
+# MANUAL DE USO: preencha os campos onde o comentario possua * e letras maiusculas
+
 from playwright.sync_api import sync_playwright
 import pyautogui
 import time
@@ -8,13 +11,17 @@ with sync_playwright() as p:
     page = browser.new_page()
     page.goto('https://www.instagram.com/')
     page.locator('//*[@id="loginForm"]/div/div[1]/div/label/input').click()
+
     page.keyboard.type("*SEU LOGIN*")  # Seu login
+
     page.keyboard.press("Tab")
+
     page.keyboard.type("*SUA SENHA*")  # Sua senha
+
     page.keyboard.press("Enter")
     time.sleep(5)
-    
-    page.goto("*URL DA PUB*") # link da pub em que o programa ira rodar
+
+    page.goto("*URL DA PUB*")  # link da pub em que o programa ira rodar
     time.sleep(3)
 # Interage com as postagens
 
@@ -23,10 +30,10 @@ with sync_playwright() as p:
     i = 1
     while i <= 5:  # Loop de comentarios Ex. aqui ele ira comentar 5 vezes
         page.get_by_placeholder("Adicione um comentário...").click()
-        page.keyboard.type("*COMENTARIO*")  # comentario desejado
+        page.keyboard.type("*COMENTARIO*")  # Comentario desejado
         page.keyboard.press("Enter")
         i = i + 1
-    time.sleep(20000) # Mantem o programa aberto até 20000 segundos
+    time.sleep(20000)  # Mantem o programa aberto até 20000 segundos
 
 # Fecha o navegador
 browser.close()
